ed9e8881616a85a90586a0b740f61479e6242f96
Dianar Senior <nonstopbiznes@gmail.com>
2024-01-14 05:45:07 +0300

    add .env

--- service/service/settings.py
+++ service/service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s49&8-&o!ozc_xgj6cd5g-&au(kq=(39sich4^o3ih7e84c+z+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
